1be174d73747fa059a642e5f576d43c79b77d8b2
Alexod24 <alex.od2408@gmail.com>
2024-09-14 22:39:15 -0500

    Cambios en el website-03

---
+++ Website-03/index.php
@@ -0,0 +1,202 @@
+<?php
+    require 'header.php';
+?>
+
+    <!-- Hero Section Begin -->
+    <section class="hero-section set-bg" data-setbg="img/5.png">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-7">
+                    <div class="hero-text">
+                        <span>Emprecemos con tu asesoria ahora mismo</span>
+                        <h2>Consultas Legales <br /> a Tu Alcance</h2>
+                        <a href="#" class="primary-btn">Generar Consulta</a>
+                    </div>
+                </div>
+                <div class="col-lg-5">
+                    <img src="img/model.png" alt="">
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Hero Section End -->
+
+    <!-- Counter Section Begin -->
+    <section class="counter-section bg-gradient">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-4">
+                    <div class="counter-text">
+                        <span>Prueba Gratuita</span>
+                        <h3>Registrate para <br />usar Arxatec GRATIS</h3>
+                    </div>
+                </div>
+                <div class="col-lg-8">
+                    <div class="cd-timer" id="countdown">
+                        <div class="cd-item">
+                            <span>40</span>
+                            <p>Days</p>
+                        </div>
+                        <div class="cd-item">
+                            <span>18</span>
+                            <p>Hours</p>
+                        </div>
+                        <div class="cd-item">
+                            <span>46</span>
+                            <p>Minutes</p>
+                        </div>
+                        <div class="cd-item">
+                            <span>32</span>
+                            <p>Seconds</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Counter Section End -->
+
+    <!-- Sección Sobre Nosotros Comienza -->
+        <section class="home-about-section spad">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-6">
+                        <div class="ha-pic">
+                            <img src="img/about.jpg" alt="Sobre Arxatec">
+                        </div>
+                    </div>
+                    <div class="col-lg-6">
+                        <div class="ha-text">
+                            <h2>Sobre Arxatec</h2>
+                            <p>En Arxatec, ofrecemos una plataforma en línea que conecta a personas con abogados expertos para resolver sus dudas legales desde la comodidad de su hogar. Nuestro objetivo es brindar un servicio confiable, accesible y eficiente para facilitar el acceso a la justicia a través de consultas legales rápidas y sencillas.</p>
+                            <ul>
+                                <li><span class="icon_check"></span> Consulta legal en línea</li>
+                                <li><span class="icon_check"></span> Asesoría personalizada</li>
+                                <li><span class="icon_check"></span> Soluciones rápidas y efectivas</li>
+                                <li><span class="icon_check"></span> Confidencialidad garantizada</li>
+                            </ul>
+                            <a href="#" class="ha-btn">Descubre Más</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    <!-- Sección Sobre Nosotros Termina -->
+
+
+    <?php
+    require 'hechos.php';
+    ?>
+
+    <?php
+    require 'services.php';
+    ?>
+
+<?php
+    require 'planes.php';
+    ?>
+
+<?php
+    require 'testimonios.php';
+    ?>
+<?php
+    require 'noticias.php';
+    ?>
+
+
+
+    
+    
+
+
+   
+
+
+    
+
+    <?php
+    require 'team.php';
+    ?>
+    <?php
+    require 'contacto.php';
+    ?>
+
+    <!-- Sección de Footer Comienza -->
+        <footer class="footer-section">
+            <div class="container">
+                <div class="partner-logo owl-carousel">
+                    <a href="#" class="pl-table">
+                        <div class="pl-tablecell">
+                            <img src="img/partner-logo/logo-1.png" alt="Socio 1">
+                        </div>
+                    </a>
+                    <a href="#" class="pl-table">
+                        <div class="pl-tablecell">
+                            <img src="img/partner-logo/logo-2.png" alt="Socio 2">
+                        </div>
+                    </a>
+                    <a href="#" class="pl-table">
+                        <div class="pl-tablecell">
+                            <img src="img/partner-logo/logo-3.png" alt="Socio 3">
+                        </div>
+                    </a>
+                    <a href="#" class="pl-table">
+                        <div class="pl-tablecell">
+                            <img src="img/partner-logo/logo-4.png" alt="Socio 4">
+                        </div>
+                    </a>
+                    <a href="#" class="pl-table">
+                        <div class="pl-tablecell">
+                            <img src="img/partner-logo/logo-5.png" alt="Socio 5">
+                        </div>
+                    </a>
+                    <a href="#" class="pl-table">
+                        <div class="pl-tablecell">
+                            <img src="img/partner-logo/logo-6.png" alt="Socio 6">
+                        </div>
+                    </a>
+                </div>
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="footer-text">
+                            <div class="ft-logo">
+                                <a href="#" class="footer-logo"><img src="img/logarxa1.png" alt="Logo Arxatec"></a>
+                            </div>
+                            <ul>
+                                <li><a href="#">Inicio</a></li>
+                                <li><a href="#">Servicios</a></li>
+                                <li><a href="#">Consultas</a></li>
+                                <li><a href="#">Blog</a></li>
+                                <li><a href="#">Contacto</a></li>
+                            </ul>
+                            <div class="copyright-text">
+                                <p> 
+                                    Copyright &copy;<script>document.write(new Date().getFullYear());</script> Todos los derechos reservados 
+                                </p>
+                            </div>
+                            <div class="ft-social">
+                                <a href="https://www.facebook.com/DerechoArbitrajeEmpresa" target="_blank"><i class="fa fa-facebook"></i></a>
+                                <a href=""><i class="fa fa-twitter"></i></a>
+                                <a href="#"><i class="fa fa-linkedin"></i></a>
+                                <a href="https://www.instagram.com/grupo_aguirre_/" target="_blank"><i class="fa fa-instagram"></i></a>
+                                <a href="https://www.youtube.com/@grupoaguirre" target="_blank"><i class="fa fa-youtube-play"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    <!-- Sección de Footer Termina -->
+
+
+    <!-- Js Plugins -->
+    <script src="js/jquery-3.3.1.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.magnific-popup.min.js"></script>
+    <script src="js/jquery.countdown.min.js"></script>
+    <script src="js/jquery.slicknav.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/main.js"></script>
+</body>
+
+</html>
